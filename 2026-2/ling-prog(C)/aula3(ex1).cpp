2613fb8fbf47d48160edf4e0d1be4199b7245f94
Gustavo Ilescas <gustavoilescas1133@gmail.com>
2026-09-16 17:33:18 -0300

    adicionando arquivos referente a trabalho de desenvolvimento web

--- 2026-2/ling-prog(C)/aula3(ex1).cpp
+++ 2026-2/ling-prog(C)/aula3(ex1).cpp
@@ -3,21 +3,13 @@
 
 int main()
 {
-   /* Elaborar um programa que apresente o valor de uma potencia de uma base (N) qualquer elevada a um expoente (M) qualquer, ou seja, de NM */
-   int N, M, i;
-   long long potencia = 1;
-
-   printf("Digite a base: ");
-   scanf("%d", &N);
-   printf("Digite o expoente: ");
-   scanf("%d", &M);
-
-   for (i = 0; i < M; i++)
-   {
-       potencia *= N;
-   }
-
-   printf("O valor de %d^%d e: %lld\n", N, M, potencia);
-   system("pause");
-   return 0;
-}
+    int soma = 0;
+    for (int i = 1; i <= 10; i++)
+    {
+        soma += i;
+        printf ("%d + %d = ", soma, i);
+        printf ("%d\n", soma);
+    }
+    printf("O total da soma dos dez primeiros nemeros inteiros e: %d\n", soma);
+    system("pause");
+    return 0;
